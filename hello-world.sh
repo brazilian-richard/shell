78fae0c59570e441c141d56590022b4782aad6f6
Richard da Silva <132865277+brazilian-richard@users.noreply.github.com>
2025-08-02 17:43:50 -0300

    Add files via upload

---
+++ hello-world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: Richard da Silva
+# GitHub: https://github.com/brazilian-richard/shell/blob/main/hello-world.sh
+# Creation date: 08/01/2025
+
+DATE=`date +%D`
+USR=`whoami`
+
+echo -e "\nHello, World!\n\n$USR | $DATE\n\a"
